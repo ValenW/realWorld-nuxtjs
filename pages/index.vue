497531e399ef160b41db70963aa369f48e7a9f92
Valen <wang_wenl@worksap.co.jp>
2020-08-12 12:27:35 +0800

    init nuxt repo

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">Home</div>
+</template>
+
+<script>
+export default {
+ name: 'index',
+  data() {
+    return {
+
+    }
+  },
+  components: {},
+  watch: {},
+  mounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
