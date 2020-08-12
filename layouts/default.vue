73ccd0fa6b99737d65ae581d2cc04b07e1ae4ef1
Valen <wang_wenl@worksap.co.jp>
2020-08-12 17:30:30 +0800

    update header bar link

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,24 +3,35 @@
     <!-- header -->
     <nav class="navbar navbar-light">
       <div class="container">
-        <a class="navbar-brand" href="index.html">conduit</a>
+        <nuxt-link class="navbar-brand" to="/">conduit</nuxt-link>
+
         <ul class="nav navbar-nav pull-xs-right">
           <li class="nav-item">
             <!-- Add "active" class when you're on that page" -->
-            <a class="nav-link active" href>Home</a>
+            <nuxt-link class="nav-link" to="/">Home</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href>
+            <nuxt-link class="nav-link" to="/editor">
               <i class="ion-compose"></i>&nbsp;New Post
-            </a>
+            </nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href>
+            <nuxt-link class="nav-link" to="/settings">
               <i class="ion-gear-a"></i>&nbsp;Settings
-            </a>
+            </nuxt-link>
+          </li>
+
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/login">Sign in</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href>Sign up</a>
+            <nuxt-link class="nav-link" to="/register">Sign up</nuxt-link>
+          </li>
+
+          <li class="nav-item">
+            <nuxt-link class="nav-link" to="/profile">
+              <img class="user-pic" src="https://avatars3.githubusercontent.com/u/8122612" />Valen
+            </nuxt-link>
           </li>
         </ul>
       </div>
