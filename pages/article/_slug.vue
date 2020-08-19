76a76986b5e71841dc31dbc7743bc5e6729f058a
Valen <wang_wenl@worksap.co.jp>
2020-08-19 18:42:40 +0800

    implement comment of articles

--- pages/article/_slug.vue
+++ pages/article/_slug.vue
@@ -18,55 +18,7 @@
         <ArticleMeta :article="article" />
       </div>
 
-      <div class="row">
-        <div class="col-xs-12 col-md-8 offset-md-2">
-          <form class="card comment-form">
-            <div class="card-block">
-              <textarea class="form-control" placeholder="Write a comment..." rows="3"></textarea>
-            </div>
-            <div class="card-footer">
-              <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              <button class="btn btn-sm btn-primary">Post Comment</button>
-            </div>
-          </form>
-
-          <div class="card">
-            <div class="card-block">
-              <p
-                class="card-text"
-              >With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <a href class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              </a>
-              &nbsp;
-              <a href class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-            </div>
-          </div>
-
-          <div class="card">
-            <div class="card-block">
-              <p
-                class="card-text"
-              >With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-            <div class="card-footer">
-              <a href class="comment-author">
-                <img src="http://i.imgur.com/Qr71crq.jpg" class="comment-author-img" />
-              </a>
-              &nbsp;
-              <a href class="comment-author">Jacob Schmidt</a>
-              <span class="date-posted">Dec 29th</span>
-              <span class="mod-options">
-                <i class="ion-edit"></i>
-                <i class="ion-trash-a"></i>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ArticleComments :article="article" />
     </div>
   </div>
 </template>
@@ -74,6 +26,7 @@
 <script>
 import { getArticle } from "@/api/article";
 import ArticleMeta from "./components/article-meta";
+import ArticleComments from "./components/article-comments";
 import MarkdownIt from "markdown-it";
 
 export default {
@@ -94,14 +47,13 @@ export default {
   },
   components: {
     ArticleMeta,
+    ArticleComments,
   },
   head() {
     return {
       title: `${this.article.title} - RealWorld`,
-      meta: [
-        { hid: "description", name: "description", content: this.article.description }
-      ]
-    }
+      meta: [{ hid: "description", name: "description", content: this.article.description }],
+    };
   },
   watch: {},
   mounted() {},
